359455e5d8915454803344fd99fe18734e58b9ee
SeyedNavidHashemiJazi <123193254+SeyedNavidHashemi@users.noreply.github.com>
2025-10-27 18:25:15 +0330

    Delete project-5-shared-memory/src directory

--- project-5-shared-memory/src/reentrantlock.h
+++
@@ -1,5 +0,0 @@
-struct reentrantlock{
-    struct spinlock lock;    // Underlying spinlock for atomicity
-    struct proc *owner;      // Current owner of the lock
-    int recursion;           // Recursion depth for reentry
-};
